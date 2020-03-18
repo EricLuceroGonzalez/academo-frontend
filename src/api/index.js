64252cbdecd688105a036daf2709c9b975979749
Eric Lucero Gonzalez <ericlucerogonzalez@gmail.com>
2020-03-18 18:07:34 -0500

    added api endpoint

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://academo-backend.herokuapp.com/"
+  baseURL: "https://academo-backend.herokuapp.com/api"
   // baseURL: "http://localhost:3001/api"
 });
 
